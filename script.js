e2c72473edeaeac38660ad52440379e53e2b0fe4
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-07-16 17:39:43 -0300

    created presentation and timer

---
+++ script.js
@@ -0,0 +1,43 @@
+const eMonths = document.getElementById("months")
+const eDays = document.getElementById("days")
+const eHours = document.getElementById("hours")
+const eMinutes = document.getElementById("minutes")
+const eSeconds = document.getElementById("seconds")
+
+const targetDate = new Date("Septembre 21, 2024 21:00:00").getTime();
+
+function formatNumber(number) {
+    return number < 10 ? '0' + number : number;
+}
+
+function updateCountdown() {
+    
+    const now = new Date().getTime().toFixed()
+    const distance = targetDate - now;
+    console.log(distance);
+    if (distance < 0) {
+        clearInterval(interval);
+        daysElement.innerText = '00';
+        hoursElement.innerText = '00';
+        minutesElement.innerText = '00';
+        secondsElement.innerText = '00';
+        return;
+    }
+
+    const months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    eMonths.innerText = formatNumber(months)
+    eDays.innerText = formatNumber(days);
+    eHours.innerText = formatNumber(hours);
+    eMinutes.innerText = formatNumber(minutes);
+    eSeconds.innerText = formatNumber(seconds);
+}
+
+window.onload = function() {
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
+};
